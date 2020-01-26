727e84934fae09804ea0887c0234eb4f9db52fc6
yajamon <yajamon.tatsuki@gmail.com>
2020-01-27 00:21:29 +0900

    Resultに適応した型を返す関数を定義

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-export type Ok<T> = {
+export type ResultOk<T> = {
+  isError: false;
   value: T;
 };
 
-export type Err<E> = {
+export type ResultErr<E> = {
+  isError: true;
   error: E;
-}
+};
+
+export type Result<T, E> = ResultOk<T> | ResultErr<E>;
+
+export const Ok: <T>(value: T) => ResultOk<T> = v => ({
+  isError: false,
+  value: v
+});
+export const Err: <E>(error: E) => ResultErr<E> = e => ({
+  isError: true,
+  error: e
+});
