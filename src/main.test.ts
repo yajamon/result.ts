ab0a6a5da3b41a4e08ca90c240ad8ff1b2a6bbac
yajamon <yajamon.tatsuki@gmail.com>
2022-04-29 02:54:43 +0900

    Add test for err()
    deprecated となった Err() のためのテストも、一応残す。

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,4 +1,4 @@
-import { Err, Ok, ok, Result, unwrap } from "./main";
+import { err, Err, Ok, ok, Result, unwrap } from "./main";
 
 describe("Ok", () => {
   it("is return ResultOk", () => {
@@ -30,6 +30,18 @@ describe("ok", () => {
     if (success.isError === false) {
       // isError を確認することで、スコープ内の型を ResultからResultOkに推定できる
       expect(success.value).toEqual("success");
+    } else {
+      throw "Wrong!";
+    }
+  });
+});
+
+describe("err", () => {
+  it("is return ResultErr", () => {
+    const error = err("error") as Result<never, string>;
+    if (error.isError) {
+      // isError を確認することで、スコープ内の型を ResultからResultErrに推定できる
+      expect(error.error).toEqual("error");
     } else {
       throw "Wrong!";
     }
